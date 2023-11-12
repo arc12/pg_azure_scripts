85aa19137ad7e7314f78e7e686b6e331d9021260
Adam Cooper <arc@hilltop-cottage.info>
2023-11-12 18:14:42 +0000

    deploy tweaks

--- Python/deploy.py
+++ Python/deploy.py
@@ -1,12 +1,14 @@
 # upload = deployment of all specified playthings to one function app
 # relies on zip file created by pre_deploy.py
 
-# from profile.publishsettings (Azure portal)
-deployment_user = "$dlpg-test1"
-# pwd stored in env variable DLPGTEST1_DEPLOY_PWD
-# app_name = "dlpg-test1"
-deploy_url = "https://dlpg-test1.scm.azurewebsites.net/api/zipdeploy"
+# from profile.publishsettings (Azure portal function app > Deployment Center)
+app_name = "dlpg-demo"
+deployment_user = "$dlpg-demo"
+deploy_password_env = "DLPGDEMO_DEPLOY_PWD"  # environment var containing the deployment password
+
+deploy_url = f"https://{app_name}.scm.azurewebsites.net/api/zipdeploy"
 zip_path = "../DEPLOY.zip"
+
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -14,7 +16,7 @@ import json
 from time import sleep
 from os import environ
 
-auth = HTTPBasicAuth(deployment_user, environ["DLPGTEST1_DEPLOY_PWD"])  # exception if not set is desired
+auth = HTTPBasicAuth(deployment_user, environ[deploy_password_env])  # will get 401 for GET below if not set is desired
 
 # check existing deployments and allow bail out
 resp = requests.get(deploy_url.replace("zipdeploy", "deployments"),  auth=auth)
@@ -37,15 +39,22 @@ if proceed:
             exit()
         print(json.dumps(dict(resp.headers), indent=2))
         poll_url = resp.headers.get("Location", None)
-        print("Log URL: ", resp.log_url)
 
     # poll progress
     complete = False
+    initial = True
     while not complete:
         resp = requests.get(poll_url,  auth=auth)
         progress = json.loads(resp.text)
         if isinstance(progress, list):
             progress = progress[0]
+        if initial:
+            log_url = progress.get("log_url", None)
+            if log_url is not None:
+                print("*" * 50)
+                print("Log URL: ", log_url)
+                print("*" * 50)
+                initial = False
         print(progress["status_text"])
         print(progress["message"])
         print(progress["progress"])
